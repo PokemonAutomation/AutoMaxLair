afa2aee42f5559f2ef47c3768092ef9f0fa2b95e
ercdndrs <55447330+ercdndrs@users.noreply.github.com>
2021-01-03 18:30:18 -0500

    Add files via upload

---
+++ Pokemon_Data/Spanish/Move.py
@@ -0,0 +1,26 @@
+# Move
+#   Eric Donders
+#   2020-11-27
+import copy
+
+class Move():
+    def __init__(self, Name, Type, Category, Power, Accuracy, PP, TM, Effect, Probability, is_spread=False, correction_factor=1):
+        self.name = Name
+        self.type = Type
+        self.category = Category
+        self.base_power = Power
+        self.accuracy = Accuracy
+        self.PP = PP
+        self.TM = TM
+        self.effect = Effect
+        self.probability = Probability
+        self.is_spread = is_spread
+        self.correction_factor = correction_factor
+
+        self.power = Power*correction_factor
+
+    def __str__(self):
+        return self.name
+
+    def __copy__(self):
+        return type(self)(self.name, self.type, self.category, self.base_power, self.accuracy, copy.deepcopy(self.PP), self.TM, self.effect, self.probability, self.is_spread, self.correction_factor)
