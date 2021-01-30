3a9d2868577aa907d72e50bc83ebf70620cad227
pifopi <dottel.gael@gmail.com>
2021-01-30 15:10:25 +0100

    add timers to display how long it took to output pickles files

--- scripts/build_all_data_files.py
+++ scripts/build_all_data_files.py
@@ -1,6 +1,14 @@
 import package_pokemon
 import score_pokemon
+import time
 
 if __name__ == '__main__':
+    start = time.time()
     package_pokemon.main()
+    end = time.time()
+    print(f'package_pokemon took {end - start}')
+    
+    start = time.time()
     score_pokemon.main()
+    end = time.time()
+    print(f'score_pokemon took {end - start}')
