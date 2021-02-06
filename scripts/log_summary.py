653929284bfb1a4f029f4c4eaaa3359afdb0f19f
ercdndrs <eric.donders@outlook.com>
2021-02-06 13:08:40 -0500

    Update log_summary.py

--- scripts/log_summary.py
+++ scripts/log_summary.py
@@ -7,7 +7,15 @@ log_path = os.path.join(base_dir, 'logs')
 file_list = os.listdir(log_path)
 
 # Modify this list with whatever bosses you want to look at.
-bosses = ['dialga', 'zapdos', 'entei']
+bosses = ['articuno', 'zapdos', 'moltres', 'mewtwo', 'raikou', 'entei',
+    'suicune', 'lugia', 'ho-oh', 'latias', 'latios', 'kyogre', 'groudon',
+    'rayquaza', 'uxie', 'mesprit', 'azelf', 'dialga', 'palkia', 'heatran',
+    'giratina', 'cresselia', 'tornadus', 'thundurus', 'reshiram', 'zekrom',
+    'landorus', 'kyurem', 'xerneas', 'yveltal', 'zygarde', 'koko', 'lele',
+    'bulu', 'fini', 'solgaleo', 'lunala', 'necrozma', 'nihilego', 'xurkitree',
+    'buzzwole', 'pheromosa', 'celesteela', 'kartana', 'guzzlord', 'stakataka',
+    'blacephalon'
+]
 global_losses = 0
 global_wins = 0
 
@@ -24,12 +32,16 @@ for boss in bosses:
             with open(os.path.join(log_path, fn), newline='\n', encoding='utf-8') as log_file:
                 num_losses = 0
                 num_wins = 0
-                for row in log_file.readlines():
-                    # The following lines are logged once at the end of a run.
-                    if 'You lose' in row:
-                        num_losses += 1
-                    if 'Congratulations' in row:
-                        num_wins += 1
+                try:
+                    for row in log_file.readlines():
+                        # The following lines are logged once at the end of a run.
+                        if 'You lose' in row:
+                            num_losses += 1
+                        if 'Congratulations' in row:
+                            num_wins += 1
+                except Exception as e:
+                    print(f'Error processing {fn}:')
+                    print(e)
                 total_losses += num_losses
                 total_wins += num_wins
 
