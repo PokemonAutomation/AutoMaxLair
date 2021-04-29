b51098be6a77478980b37b3e17b3edde3de58006
ercdndrs <eric.donders@outlook.com>
2021-04-29 14:00:26 -0400

    Fix linting errors

--- scripts/OpenCV_demo_staticimage.py
+++ scripts/OpenCV_demo_staticimage.py
@@ -18,6 +18,7 @@ high_H_name = 'High H'
 high_S_name = 'High S'
 high_V_name = 'High V'
 
+
 def on_low_H_thresh_trackbar(val):
     global low_H
     global high_H
@@ -69,12 +70,30 @@ def on_high_V_thresh_trackbar(val):
 cap = cv.imread(filename)
 cv.namedWindow(window_capture_name)
 cv.namedWindow(window_detection_name)
-cv.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
-cv.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
-cv.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
-cv.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
-cv.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
-cv.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
+cv.createTrackbar(
+    low_H_name, window_detection_name, low_H, max_value_H,
+    on_low_H_thresh_trackbar
+)
+cv.createTrackbar(
+    high_H_name, window_detection_name, high_H, max_value_H,
+    on_high_H_thresh_trackbar
+)
+cv.createTrackbar(
+    low_S_name, window_detection_name, low_S, max_value,
+    on_low_S_thresh_trackbar
+)
+cv.createTrackbar(
+    high_S_name, window_detection_name, high_S, max_value,
+    on_high_S_thresh_trackbar
+)
+cv.createTrackbar(
+    low_V_name, window_detection_name, low_V, max_value,
+    on_low_V_thresh_trackbar
+)
+cv.createTrackbar(
+    high_V_name, window_detection_name, high_V, max_value,
+    on_high_V_thresh_trackbar
+)
 while True:
 
     frame = cap
@@ -83,7 +102,8 @@ while True:
         print('No frame found!')
         break
     frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    frame_threshold = cv.inRange(
+        frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
 
     cv.imshow(window_capture_name, frame)
     cv.imshow(window_detection_name, frame_threshold)
